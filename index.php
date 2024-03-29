b9c4282c665b2dc1615e83b5caa230f9b63989c4
LuminovaGameStudios <161859760+LuminovaGameStudios@users.noreply.github.com>
2024-03-30 03:48:37 +0800

    kaya lang 'to ganto kasi di ko marun sa xampp

--- index.php
+++ index.php
@@ -2,13 +2,16 @@
 $sqlTable = $_GET['table'];
 $sqlCommand;
 if($sqlTable == 'TBL_StudentData') {
-    $sqlCommand = "INSERT INTO $sqlTable (UID, Strand, Section) VALUES ($_GET['uid'], $_GET['strand'], $_GET['section'])";
+    // $sqlCommand = "INSERT INTO $sqlTable (UID, Strand, Section) VALUES ($_GET['uid'], $_GET['strand'], $_GET['section'])";
+    print_r(1);
 } elseif($sqlTable == 'TBL_PretestAnswers' || $sqlTable == 'TBL_PosttestAnswers') {
-    $sqlCommand = "INSERT INTO $sqlTable (UID, Q1, Q2, Q3, Q4, Q5, Q6, Q7, Q8, Q9, Q10) VALUES ($_GET['uid'], $_GET['q1'], $_GET['q2'], $_GET['q3'], $_GET['q4'], $_GET['q5'], $_GET['q6'], $_GET['q7'], $_GET['q8'], $_GET['q9'], $_GET['q10'])";
+    // $sqlCommand = "INSERT INTO $sqlTable (UID, Q1, Q2, Q3, Q4, Q5, Q6, Q7, Q8, Q9, Q10) VALUES ($_GET['uid'], $_GET['q1'], $_GET['q2'], $_GET['q3'], $_GET['q4'], $_GET['q5'], $_GET['q6'], $_GET['q7'], $_GET['q8'], $_GET['q9'], $_GET['q10'])";
+    print_r(2);
 } else {
-    echo 'error: no table';
+    // echo 'error: no table';
+    print_r(3);
 }
-print_r($sqlCommand);
+// print_r($sqlCommand);
 // try {
 //     $conn = new PDO("sqlsrv:server = tcp:matturns.database.windows.net,1433; Database = Matturns", "matturns_admin", "ap#rvBNFTi7KpZ");
 //     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
