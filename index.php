6b18f9c222dc01e103b87a629f72e37cd7006b90
LuminovaGameStudios <161859760+LuminovaGameStudios@users.noreply.github.com>
2024-03-29 17:03:24 +0800

    29/03/24 part 2

--- index.php
+++ index.php
@@ -2,6 +2,22 @@
 try {
     $conn = new PDO("sqlsrv:server = tcp:matturns.database.windows.net,1433; Database = Matturns", "matturns_admin", "ap#rvBNFTi7KpZ");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    try {
+        $sql = "SELECT * FROM TBL_StudentData";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+    
+        $results = $stmt->fetchAll();
+    
+        print_r($results);
+    }
+    catch (PDOException $e) {
+        print("Error querying the database.");
+        die(print_r($e));
+    }
+    
+    $conn = null;
 }
 catch (PDOException $e) {
     print("Error connecting to SQL Server.");
