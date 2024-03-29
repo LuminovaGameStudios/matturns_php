f5d8529aeefc9fd2046592dcff3298f89027dd57
LuminovaGameStudios <161859760+LuminovaGameStudios@users.noreply.github.com>
2024-03-29 16:43:16 +0800

    29/03/24

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
-
-echo "Hello World!";
+try {
+    $conn = new PDO("sqlsrv:server = tcp:matturns.database.windows.net,1433; Database = Matturns", "matturns_admin", "ap#rvBNFTi7KpZ");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+?>
