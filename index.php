a2f6c9c25a8b834cb254d67093957de5be10a126
LuminovaGameStudios <161859760+LuminovaGameStudios@users.noreply.github.com>
2024-03-30 04:46:40 +0800

    legit ba, yun yung prob?

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $sqlCommand;
 if($sqlTable == 'TBL_StudentData') {
     $sqlCommand = "INSERT INTO " . $sqlTable . " (UID, Strand, Section) VALUES (" . $_GET['uid'] . ", " . $_GET['strand'] . ", " . $_GET['section'] . ")"; 
 } elseif($sqlTable == 'TBL_PretestAnswers' || $sqlTable == 'TBL_PosttestAnswers') {
-    $sqlCommand = "INSERT INTO " . $sqlTable . " (UID, Q1, Q2, Q3, Q4, Q5, Q6, Q7, Q8, Q9, Q10) VALUES (" . $_GET['uid'] . ", " . $_GET['q1'] . ", " . $_GET['q2'] . ", " . $_GET['q3'] . ", " . $_GET['q4'] . ", " . $_GET['q5'] . ", " . $_GET['q6'] . ", " . $_GET['q7'] . ", " . $_GET['q8'] . ", " . $_GET['q9'] . ", " .   . $_GET['q10'] . ")";
+    $sqlCommand = "INSERT INTO " . $sqlTable . " (UID, Q1, Q2, Q3, Q4, Q5, Q6, Q7, Q8, Q9, Q10) VALUES (" . $_GET['uid'] . ", " . $_GET['q1'] . ", " . $_GET['q2'] . ", " . $_GET['q3'] . ", " . $_GET['q4'] . ", " . $_GET['q5'] . ", " . $_GET['q6'] . ", " . $_GET['q7'] . ", " . $_GET['q8'] . ", " . $_GET['q9'] . ", " . $_GET['q10'] . ")";
 } else {
     echo 'error: no table';
 }
