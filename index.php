b3c44c6dc4e90839d7baaa323ccd5aa4631b3c3d
LuminovaGameStudios <161859760+LuminovaGameStudios@users.noreply.github.com>
2024-03-29 23:17:05 +0800

    test

--- index.php
+++ index.php
@@ -1,26 +1,28 @@
 <?php
-try {
-    $conn = new PDO("sqlsrv:server = tcp:matturns.database.windows.net,1433; Database = Matturns", "matturns_admin", "ap#rvBNFTi7KpZ");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// try {
+//     $conn = new PDO("sqlsrv:server = tcp:matturns.database.windows.net,1433; Database = Matturns", "matturns_admin", "ap#rvBNFTi7KpZ");
+//     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    try {
-        $sql = "SELECT * FROM TBL_StudentData";
-        $stmt = $conn->prepare($sql);
-        $stmt->execute();
+//     try {
+//         $sql = "SELECT * FROM TBL_StudentData";
+//         $stmt = $conn->prepare($sql);
+//         $stmt->execute();
     
-        $results = $stmt->fetchAll();
+//         $results = $stmt->fetchAll();
     
-        print_r($results);
-    }
-    catch (PDOException $e) {
-        print("Error querying the database.");
-        die(print_r($e));
-    }
+//         print_r($results);
+//     }
+//     catch (PDOException $e) {
+//         print("Error querying the database.");
+//         die(print_r($e));
+//     }
     
-    $conn = null;
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
+//     $conn = null;
+// }
+// catch (PDOException $e) {
+//     print("Error connecting to SQL Server.");
+//     die(print_r($e));
+// }
+
+print_r($_GET['name']);
 ?>
