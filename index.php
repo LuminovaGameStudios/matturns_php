7f8e1f0426ca68f7c543af9a19db1a84f1b74e91
LuminovaGameStudios <161859760+LuminovaGameStudios@users.noreply.github.com>
2024-03-30 05:34:12 +0800

    balik testing

--- index.php
+++ index.php
@@ -9,59 +9,64 @@ if($sqlTable == 'TBL_StudentData') {
     echo 'error: no table';
 }
 
-try {
-    $conn = new PDO("sqlsrv:server = tcp:matturns.database.windows.net,1433; Database = Matturns", "matturns_admin", "ap#rvBNFTi7KpZ");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+echo sqlTable . " " . var_dump(sqlTable) . "<br>";
+echo $_GET['uid'] . " " . var_dump($_GET['uid']) . "<br>";
+echo $_GET['strand'] . " " . var_dump($_GET['strand']) . "<br>";
+echo $_GET['section'] . " " . var_dump($_GET['section']) . "<br>";
 
-    try {
-        $stmt = $conn->prepare($sqlCommand);
-         if ($sqlTable == 'TBL_StudentData') {
-            $uid = $_GET['uid'];
-            $strand = $_GET['strand'];
-            $section = intval($_GET['section']);
+// try {
+//     $conn = new PDO("sqlsrv:server = tcp:matturns.database.windows.net,1433; Database = Matturns", "matturns_admin", "ap#rvBNFTi7KpZ");
+//     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-            $stmt->bindValue(1, $uid);
-            $stmt->bindValue(2, $strand);
-            $stmt->bindValue(3, $section);
+//     try {
+//         $stmt = $conn->prepare($sqlCommand);
+//          if ($sqlTable == 'TBL_StudentData') {
+//             $uid = $_GET['uid'];
+//             $strand = $_GET['strand'];
+//             $section = intval($_GET['section']);
 
-            $stmt->execute();
-        } elseif ($sqlTable == 'TBL_PretestAnswers' || $sqlTable == 'TBL_PosttestAnswers') {
-            $uid = $_GET['uid'];
-            $q1 = $_GET['q1'];
-            $q2 = $_GET['q2'];
-            $q3 = $_GET['q3'];
-            $q4 = $_GET['q4'];
-            $q5 = $_GET['q5'];
-            $q6 = $_GET['q6'];
-            $q7 = $_GET['q7'];
-            $q8 = $_GET['q8'];
-            $q9 = $_GET['q9'];
-            $q10 = $_GET['q10'];
+//             $stmt->bindValue(1, $uid);
+//             $stmt->bindValue(2, $strand);
+//             $stmt->bindValue(3, $section);
 
-            $stmt->bindValue(1, $uid);
-            $stmt->bindValue(2, $q1);
-            $stmt->bindValue(3, $q2);
-            $stmt->bindValue(4, $q3);
-            $stmt->bindValue(5, $q4);
-            $stmt->bindValue(6, $q5);
-            $stmt->bindValue(7, $q6);
-            $stmt->bindValue(8, $q7);
-            $stmt->bindValue(9, $q8);
-            $stmt->bindValue(10, $q9);
-            $stmt->bindValue(11, $q10);
+//             $stmt->execute();
+//         } elseif ($sqlTable == 'TBL_PretestAnswers' || $sqlTable == 'TBL_PosttestAnswers') {
+//             $uid = $_GET['uid'];
+//             $q1 = $_GET['q1'];
+//             $q2 = $_GET['q2'];
+//             $q3 = $_GET['q3'];
+//             $q4 = $_GET['q4'];
+//             $q5 = $_GET['q5'];
+//             $q6 = $_GET['q6'];
+//             $q7 = $_GET['q7'];
+//             $q8 = $_GET['q8'];
+//             $q9 = $_GET['q9'];
+//             $q10 = $_GET['q10'];
 
-            $stmt->execute();
-        }
-    }
-    catch (PDOException $e) {
-        print("Error querying the database.");
-        die(print_r($e));
-    }
+//             $stmt->bindValue(1, $uid);
+//             $stmt->bindValue(2, $q1);
+//             $stmt->bindValue(3, $q2);
+//             $stmt->bindValue(4, $q3);
+//             $stmt->bindValue(5, $q4);
+//             $stmt->bindValue(6, $q5);
+//             $stmt->bindValue(7, $q6);
+//             $stmt->bindValue(8, $q7);
+//             $stmt->bindValue(9, $q8);
+//             $stmt->bindValue(10, $q9);
+//             $stmt->bindValue(11, $q10);
+
+//             $stmt->execute();
+//         }
+//     }
+//     catch (PDOException $e) {
+//         print("Error querying the database.");
+//         die(print_r($e));
+//     }
     
-    $conn = null;
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
-?>
+//     $conn = null;
+// }
+// catch (PDOException $e) {
+//     print("Error connecting to SQL Server.");
+//     die(print_r($e));
+// }
+// ?>
